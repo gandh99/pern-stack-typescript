e881b022cd3b33c40effd18a59cbc1ce4fd4cdaf
gandh99 <gandh99@hotmail.com>
2020-05-17 12:48:31 +0800

    Redesigned login page

--- client/src/login/Background.tsx
+++ client/src/login/Background.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import BackgroundImage from '../images/background.jpeg'
 
 export default function Background() {
     const classes = useStyles()
@@ -14,9 +13,6 @@ export default function Background() {
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        // background: 'linear-gradient(326deg, rgba(113,106,154,1) 15%, rgba(137,128,185,1) 78%)', 
-        // backgroundImage: `url(${BackgroundImage})`,
-        // backgroundSize: 'cover',
         backgroundImage: 'linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)',
         opacity: 1,
         position: 'fixed',
